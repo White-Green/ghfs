3006b766b0676311002a39cd5d34de69dc1969bc
White-Green <43771790+White-Green@users.noreply.github.com>
2020-11-10 20:55:12 +0900

    read/write token

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::io::Write;
+use std::fs::Permissions;
+use std::io::{Error, Write};
+use std::path::Path;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg, ArgMatches, SubCommand};
 use regex::Regex;
 use reqwest::blocking::Client;
+use users::get_current_uid;
 
 use crate::github_filesystem::http::json_request;
 use crate::github_filesystem::mount;
-use users::get_current_uid;
-use std::fs::Permissions;
 
 mod github_filesystem;
 
@@ -34,49 +35,36 @@ fn main() {
     let matches = App::new("ghfs")
         .author("White-Green")
         .about("mount GitHub repository into local filesystem.")
-        .arg(Arg::with_name("URL")
-            .help("github tree URL")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::with_name("PATH")
-            .help("path to directory for mount")
-            .takes_value(true)
-            .required(true))
         .subcommand(SubCommand::with_name("token")
             .help("manage personal access token")
-            .subcommand(SubCommand::with_name("set")
-                .arg(Arg::with_name("token")
-                    .help("personal access token")
-                    .takes_value(true)
-                    .required(true)))
+            .subcommand(SubCommand::with_name("set"))
             .subcommand(SubCommand::with_name("remove")))
+        .subcommand(SubCommand::with_name("mount")
+            .help("mount a repository.")
+            .arg(Arg::with_name("URL")
+                .help("github tree URL")
+                .takes_value(true)
+                .required(true))
+            .arg(Arg::with_name("PATH")
+                .help("path to directory for mount")
+                .takes_value(true)
+                .required(true))
+        )
         .get_matches();
 
     if let Some(token_arg) = matches.subcommand_matches("token") {
-        let uid = get_current_uid();
-        let path = format!("./{}", uid);
-        if let Some(token_set_arg) = token_arg.subcommand_matches("set") {
-            // command [ghfs token set]
-            std::fs::write(&path, token_set_arg.value_of("token").unwrap())
-                .expect("write failed.");
-            let permission = <Permissions as std::os::unix::fs::PermissionsExt>::from_mode(0o600);
-            std::fs::set_permissions(&path, permission)
-                .expect("chmod failed");
-        } else if let Some(token_remove_arg) = token_arg.subcommand_matches("remove") {
-            // command [ghfs token remove]
-            std::fs::remove_file(&path);
-        } else {
-            // command [ghfs token]
-        }
+        token_subcommand(token_arg);
+        return;
     }
+    let matches = if let Some(matches) = matches.subcommand_matches("mount") {
+        matches
+    } else {
+        println!("{}", matches.usage());
+        return;
+    };
 
-    print!("GitHub Username: ");
-    std::io::stdout().flush().unwrap();
-    let mut username = String::new();
-    std::io::stdin().read_line(&mut username).unwrap();
-    let username = username.trim().to_string();
+    let (username, pass) = get_auth_info();
 
-    let pass = rpassword::prompt_password_stdout("Password: ").unwrap();
     // println!("{}:{}", username, pass);
 
     let client = Client::new();
@@ -91,4 +79,58 @@ fn main() {
     println!("url:{}", url);
     let path = matches.value_of("PATH").unwrap();
     mount(&url, client, &path, username, pass);
+}
+
+fn get_auth_info() -> (String, String) {
+    let path = get_token_file_path();
+    if let Ok(s) = std::fs::read_to_string(&path) {
+        let regex = Regex::new("^(?P<n>.+)\n(?P<p>.+)\n$").unwrap();
+        if let Some(t) = regex.captures(&s) {
+            return (t["n"].to_string(), t["p"].to_string());
+        }
+    }
+    print!("GitHub Username: ");
+    std::io::stdout().flush().unwrap();
+    let mut username = String::new();
+    std::io::stdin().read_line(&mut username).unwrap();
+    let username = username.trim().to_string();
+
+    let pass = rpassword::prompt_password_stdout("Password or Token: ").unwrap();
+    (username, pass)
+}
+
+fn token_subcommand(token_arg: &ArgMatches) {
+    let path = get_token_file_path();
+    if let Some(_) = token_arg.subcommand_matches("set") {
+        // command [ghfs token set]
+
+        print!("GitHub Username: ");
+        std::io::stdout().flush().unwrap();
+        let mut username = String::new();
+        std::io::stdin().read_line(&mut username).unwrap();
+        let username = username.trim().to_string();
+        let pass = rpassword::prompt_password_stdout("Token: ").unwrap();
+
+        std::fs::write(&path, format!("{}\n{}\n", username, pass))
+            .expect("write failed.");
+
+        let permission = <Permissions as std::os::unix::fs::PermissionsExt>::from_mode(0o600);
+        std::fs::set_permissions(&path, permission)
+            .expect("chmod failed");
+    } else if let Some(_) = token_arg.subcommand_matches("remove") {
+        // command [ghfs token remove]
+        std::fs::remove_file(&path);
+    } else {
+        // command [ghfs token]
+        if Path::new(&path).exists() {
+            println!("Token is registered.");
+        } else {
+            println!("Token is not registered.");
+        }
+    }
+}
+
+fn get_token_file_path() -> String {
+    let uid = get_current_uid();
+    format!("./{}", uid)
 }
